6b5059f9d5355e94a822021db11b9759d9d94bab
sena <senanurgercek4@gmail.com>
2026-03-31 00:33:12 +0300

    Hero bölümünü ekran yüksekliğine sabitleyip boşluğu kaldır

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
   const textY = useTransform(scrollYProgress, [0, 0.3], [0, -60]);
 
   return (
-    <section ref={ref} className="relative h-[125vh] md:h-[135vh]">
+    <section ref={ref} className="relative h-screen">
       <div className="sticky top-0 h-screen flex items-center justify-center overflow-hidden">
         {/* Ambient glow */}
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-primary/8 rounded-full blur-[200px]" />
